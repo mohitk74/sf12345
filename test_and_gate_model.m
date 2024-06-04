ede69831c8087e0d24edcf66c88315354d33ab8f
mohitk74 <163093956+mohitk74@users.noreply.github.com>
2024-06-04 15:06:28 +0530

    Update test_and_gate_model.m

--- test_and_gate_model.m
+++ test_and_gate_model.m
@@ -1,32 +1,3 @@
-modelName = 'and_gate_model';
-load_system(modelName);
-
-open_system(modelName);
-
-set_param(modelName, 'SystemTargetFile', 'ert.tlc');
-
-set_param(modelName, 'Toolchain', 'MinGW64 | gmake (64-bit Windows)');
-
-set_param(modelName, 'Solver', 'FixedStepDiscrete');
-
-set_param(modelName, 'FixedStep', '0.1');
-
-rtwbuild(modelName);
-
-checkIDs = {'mathworks.jmaab.ar_0001', 'mathworks.jmaab.ar_0002',
-            'mathworks.jmaab.jc_0241', 'mathworks.jmaab.jc_0242',
-            'mathworks.jmaab.jc_0201', 'mathworks.jmaab.jc_0211',
-            'mathworks.jmaab.jc_0231', 'mathworks.jmaab.jc_0243',
-            'mathworks.jmaab.jc_0247', 'mathworks.jmaab.jc_0244',
-            'mathworks.jmaab.jc_0222', 'mathworks.jmaab.jc_0232',
-            'mathworks.jmaab.jc_0245', 'mathworks.jmaab.jc_0246',
-            'mathworks.jmaab.jc_0795', 'mathworks.jmaab.jc_0796',
-            'mathworks.jmaab.jc_0791', 'mathworks.jmaab.jc_0792',
-            'mathworks.jmaab.jc_0700', 'mathworks.maab.na_0019'};
-
-results = ModelAdvisor.run(modelName, checkIDs);
-
-
 function tests = test_and_gate_model()
     tests = functiontests(localfunctions);
 end
